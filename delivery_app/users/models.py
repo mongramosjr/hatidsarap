3e72d5052a4dfa5c17df1283462d116074da3db7
Mong Ramos Jr. <mongramosjr@gmail.com>
2024-06-27 17:52:44 +0800

    rename the folder for saving face image and id card image

--- delivery_app/users/models.py
+++ delivery_app/users/models.py
@@ -14,16 +14,36 @@ class HatidSarapUser(AbstractUser):
     user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='customer')
     is_verified = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.username
+
+    def verify_user(self):
+        self.is_verified = True
+        self.save()
+
+    def can_place_order(self):
+        return self.is_verified and self.user_type == 'customer'
+
+    def can_accept_order(self):
+        return self.is_verified and self.user_type == 'rider'
+
+    def can_manage_store(self):
+        return self.is_verified and self.user_type == 'store'
+
+
+def user_directory_path(instance, filename):
+    return f'internal_media/user_{instance.user.id}/{filename}'
+
 
 class FaceImage(models.Model):
     user = models.OneToOneField(HatidSarapUser, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='face_images/')
+    image = models.ImageField(upload_to=user_directory_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
 class IDCardImage(models.Model):
     user = models.OneToOneField(HatidSarapUser, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='id_card_images/')
+    image = models.ImageField(upload_to=user_directory_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
 
